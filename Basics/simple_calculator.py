ae9c87b562abb1fd5aaf7a1fb0b8ae912eee6434
Pranaya Shrestha <Pranaya Shrestha>
2026-05-26 14:41:45 +0545

    python basics started

---
+++ Basics/simple_calculator.py
@@ -0,0 +1,13 @@
+#Take two numbers as input and print: 
+#addition
+#subtraction
+#multiplication
+#division
+
+a = int(input("Enter a number:"))
+b= int(input("Enter another number:"))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
