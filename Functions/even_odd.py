7c776658f7486049768e79da17a9a0687ff8c0a8
Pranaya Shrestha <sthapranaya890@gmail.com>
2026-05-27 16:24:29 +0545

    functions

---
+++ Functions/even_odd.py
@@ -0,0 +1,8 @@
+def odd_even(a):
+    if a%2==0:
+        print("Even")
+    else:
+        print("Odd")
+
+odd_even(5)
+odd_even(8)
---
+++ Functions/square.py
@@ -0,0 +1,5 @@
+def sq(a):
+    return(a**2)
+
+x=sq(6)
+print(x)
