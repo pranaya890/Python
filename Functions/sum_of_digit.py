c93546357f36da0a40d501b9759ed9bbae250435
Pranaya Shrestha <sthapranaya890@gmail.com>
2026-05-28 21:07:40 +0545

    recursion

---
+++ Functions/sum_of_digit.py
@@ -0,0 +1,12 @@
+def sum_digits(n):
+    if n == 0:
+        return 0
+    
+    return (n % 10) + sum_digits(n // 10)
+
+
+num = int(input("Enter a number: "))
+
+result = sum_digits(num)
+
+print("Sum of digits =", result)
